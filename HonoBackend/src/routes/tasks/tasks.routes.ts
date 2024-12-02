b402761169cf53a972597be25e544f436bb08556
Parmjeet <iamparmjeetmishra@gmail.com>
2024-12-02 23:41:19 +0530

    Delete Route

--- HonoBackend/src/routes/tasks/tasks.routes.ts
+++ HonoBackend/src/routes/tasks/tasks.routes.ts
@@ -92,7 +92,30 @@ export const patch = createRoute({
   },
 });
 
+export const remove = createRoute({
+  path: "/tasks/{id}",
+  tags,
+  request: {
+    params: IdParamsSchema,
+  },
+  method: "delete",
+  responses: {
+    [HttpStatusCodes.NO_CONTENT]: {
+      description: "The Task has been deleted",
+    },
+    [HttpStatusCodes.NOT_FOUND]: jsonContent(
+      notFoundSchema,
+      "Task not found",
+    ),
+    [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(IdParamsSchema),
+      "Invalid id error",
+    ),
+  },
+});
+
 export type ListRoute = typeof list;
 export type CreateRoute = typeof create;
 export type GetOneRoute = typeof getOne;
 export type PatchRoute = typeof patch;
+export type RemoveRoute = typeof remove;
