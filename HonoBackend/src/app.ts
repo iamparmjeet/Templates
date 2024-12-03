5d55bee383a8d30fe6a3cad2959254f697c9bb6e
Parmjeet <iamparmjeetmishra@gmail.com>
2024-12-03 13:07:35 +0530

    Test finish

--- HonoBackend/src/app.ts
+++ HonoBackend/src/app.ts
@@ -1,6 +1,6 @@
-import index from "@/index.route";
 import configureOpenAPI from "@/lib/configure-open-api";
 import createApp from "@/lib/create-app";
+import index from "@/routes/index.route";
 import tasks from "@/routes/tasks/tasks.index";
 
 const app = createApp();
