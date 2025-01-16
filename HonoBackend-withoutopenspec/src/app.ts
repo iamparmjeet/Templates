8fce92fb797ffb60071264a08e6f7df91c8570bd
Parmjeet <iamparmjeetmishra@gmail.com>
2025-01-16 18:56:05 +0530

    dirzzle wrangler update for nodejs compat

--- HonoBackend-withoutopenspec/src/app.ts
+++ HonoBackend-withoutopenspec/src/app.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { notFound } from "stoker/middlewares";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.notFound(notFound);
 
 export default app;
