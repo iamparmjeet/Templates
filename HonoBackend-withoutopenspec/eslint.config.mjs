2df06319827f5ad39f96549ea03c3cd386bc9480
Parmjeet <iamparmjeetmishra@gmail.com>
2025-01-16 18:29:32 +0530

    eslint setup

--- HonoBackend-withoutopenspec/eslint.config.mjs
+++ HonoBackend-withoutopenspec/eslint.config.mjs
@@ -1,27 +1,27 @@
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
 
 export default antfu({
-  type: 'app',
+  type: "app",
   typescript: true,
   formatters: true,
   stylistic: {
     indent: 2,
     semi: true,
-    quotes: 'double',
+    quotes: "double",
   },
-  ignores: ['**/migrations/*'],
+  ignores: ["**/migrations/*"],
 }, {
   rules: {
-    'no-console': ['warn'],
-    'antfu/no-top-level-await': ['off'],
-    'node/prefer-global/process': ['off'],
-    'node/no-process-env': ['error'],
-    'perfectionist/sort-imports': ['error', {
-      tsconfigRootDir: '.',
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
     }],
-    'unicorn/filename-case': ['error', {
-      case: 'kebabCase',
-      ignore: ['README.md'],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
     }],
   },
-})
+});
