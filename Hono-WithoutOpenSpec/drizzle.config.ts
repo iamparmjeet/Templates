544b66c66bda3711947eca4dd0f776a37363aca3
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-09 20:04:53 +0530

    Update=>drizzle updated for d1

--- Hono-WithoutOpenSpec/drizzle.config.ts
+++ Hono-WithoutOpenSpec/drizzle.config.ts
@@ -1,12 +1,16 @@
-import env from "@/env-runtime";
 import { defineConfig } from "drizzle-kit";
+import env from "@/env-runtime";
+
+// const env = parseEnv(process.env as any);
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  dialect: "turso",
+  dialect: "sqlite",
+  driver: "d1-http",
   dbCredentials: {
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
+    accountId: env.CF_ACCOUNT_ID,
+    databaseId: env.CF_D1_DB_ID,
+    token: env.CF_TOKEN_ID,
   },
 });
