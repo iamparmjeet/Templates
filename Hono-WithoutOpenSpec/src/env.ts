70b3471f5694d7135dc782f673dbcd88828af7cf
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-09 20:03:48 +0530

    Update=>env updated for new betterauth env

--- Hono-WithoutOpenSpec/src/env.ts
+++ Hono-WithoutOpenSpec/src/env.ts
@@ -1,47 +1,66 @@
-/* eslint-disable node/no-process-env */
-import { config } from "dotenv";
-import { expand } from "dotenv-expand";
-import path from "node:path";
 import { z } from "zod";
 
-expand(config({
-  path: path.resolve(
-    process.cwd(),
-    process.env.NODE_ENV === "test" ? ".env.test" : ".env",
-  ),
-}));
-
-const EnvSchema = z.object({
-  NODE_ENV: z.string().default("development"),
-  PORT: z.coerce.number().default(9999),
-  LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
-  DATABASE_URL: z.string().url(),
-  DATABASE_AUTH_TOKEN: z.string().optional(),
-  KINDE_CLIENT_ID: z.string(),
-  KINDE_CLIENT_SECRET: z.string(),
-  KINDE_ISSUER_URL: z.string().url(),
-  KINDE_SITE_URL: z.string().url(),
-  KINDE_POST_LOGOUT_REDIRECT_URL: z.string().url(),
-  KINDE_POST_LOGIN_REDIRECT_URL: z.string().url(),
-}).superRefine((input, ctx) => {
-  if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.invalid_type,
-      expected: "string",
-      received: "undefined",
-      path: ["DATABASE_AUTH_TOKEN"],
-      message: "DATABASE_AUTH_TOKEN is required in production",
-    });
-  }
-});
+const EnvSchema = z
+  .object({
+    NODE_ENV: z.string().default("development"),
+    PORT: z.coerce.number().default(9999),
+    LOG_LEVEL: z.enum([
+      "fatal",
+      "error",
+      "warn",
+      "info",
+      "debug",
+      "trace",
+      "silent",
+    ]),
+    LOCAL_APP: z.string(),
+    PROD_APP: z.string(),
+    CF_DB_NAME: z.string(),
+    CF_D1_DB_ID: z.string(),
+    CF_ACCOUNT_ID: z.string(),
+    CF_TOKEN_ID: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string(),
+    // Socials Providers
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+  })
+  .superRefine((input, ctx) => {
+    if (input.NODE_ENV === "production") {
+      if (!input.CF_ACCOUNT_ID) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.invalid_type,
+          expected: "string",
+          received: "undefined",
+          path: ["CF_ACCOUNT_ID"],
+          message: "CF_ACCOUNT_ID is required in production",
+        });
+      }
+      if (!input.CF_TOKEN_ID) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.invalid_type,
+          expected: "string",
+          received: "undefined",
+          path: ["CF_TOKEN_ID"],
+          message: "CF_TOKEN_ID is required in production",
+        });
+      }
+    }
+  });
 
 export type Environment = z.infer<typeof EnvSchema>;
 
-export function parseEnv(data: any) {
+export function parseEnv(data: Environment): Environment {
   const { data: env, error } = EnvSchema.safeParse(data);
 
   if (error) {
-    const errorMessage = `❌ Invalid env - ${Object.entries(error.flatten().fieldErrors).map(([key, errors]) => `${key}: ${errors.join(",")}`).join(" | ")}`;
+    const errorMessage = `❌ Invalid env - ${Object.entries(
+      error.flatten().fieldErrors
+    )
+      .map(([key, errors]) => `${key}: ${errors.join(",")}`)
+      .join(" | ")}`;
     throw new Error(errorMessage);
   }
 
